7c55008410ba5c1a40f8c9348862c08a7be82730
GuimaraesGui <ep.0.season.0@gmail.com>
2024-06-12 00:52:44 -0300

    1º commit

---
+++ Exercício_1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+"""
+A lista da Billboard Hot 100 traz as 100 músicas mais populares da semana, com:
+nome da música, artista, posição na última semana, posição na semana atual e semanas consecutivas na lista.
+
+Você extraiu esses dados para o dataframe 'billboard_100', que contem as colunas 'artista' e 'musica',
+e diversos outros dados que você deseja análisar.
+Como pode fazer para visualizar os nomes das músicas da Taylor Swift contidas na Billboard Hot 100 da semana? (apenas os nomes)
+"""
+
+new_df = pd.read_csv()
